4392d5f3098f480c0b290e447bfa9d82df29bd26
NRxl <nathan.rouxel@gmail.com>
2015-04-22 19:01:02 +0200

    merge

--- django_prelib/wsgi.py
+++ django_prelib/wsgi.py
@@ -11,8 +11,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_prelib.settings")
 
 from django.core.wsgi import get_wsgi_application
-<<<<<<< HEAD
 
-=======
->>>>>>> ba40893cfc03f3c84497840daff9d73f4c62a4e5
 application = get_wsgi_application()
