43af74516e3753f0361bcb48dac75af895631baa
NRxl <nathan.rouxel@gmail.com>
2015-04-22 20:34:40 +0200

    Correction du code add_report et see_last_report

--- report/urls.py
+++ report/urls.py
@@ -3,7 +3,7 @@ from report import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<station_id>\d{5})/(?P<broken_bikes>\d{1,2})/$', views.add_report, name='add_report'),
-    url(r'^(?P<station_id>\d{5})/$',views.see_last_report, name='see_last_report'),
+    url(r'^(?P<station_id>\d{5})/(?P<available_bikes>\d{1,2})/(?P<broken_bikes>\d{1,2})/$', views.add_report, name='add_report'),
+    url(r'^(?P<station_id>\d{5})/$', views.see_last_report, name='see_last_report'),
 )
 
--- report/views.py
+++ report/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 from report.models import Report
 from stationdict import stations
+from datetime import datetime
 import json
 
 
@@ -13,12 +14,12 @@ def index(request):
 
 
 def add_report(request, station_id, available_bikes, broken_bikes):
-    if available_bikes >= broken_bikes:
+    if int(available_bikes) >= int(broken_bikes):
         if station_id in stations:
             new_report = Report(report_date=timezone.now(),
                                 station_id=station_id,
-                                broken_bikes=broken_bikes,
-                                available_bikes=available_bikes)
+                                available_bikes=available_bikes,
+                                broken_bikes=broken_bikes)
             new_report.save()
             if(int(broken_bikes)>=2):
                 return HttpResponse("Thanks for reporting broken bikes. We note that at the moment, %s bikes out of %s are broken at station #%s."
@@ -36,21 +37,25 @@ def add_report(request, station_id, available_bikes, broken_bikes):
         return HttpResponse("Sorry, but it is absurd that %s bikes are broken out of %s at station #%s."
                                     % (broken_bikes, available_bikes, station_id))
 
-def see_last_report(request, stationId):##returns a dictionnary with data about the last report in a JSON format
-    if stationId in stations:
-        if len(Report.objects.filter(station_id=station_id).order_by('-report_date')) >= 1:
-            last_report = Report.objects.get(station_id=stationId).order_by('-report_date')[0]
-            if last_report.broken_bikes is None:
-                return HttpResponse("There is no report yet for this station.")
-            else:
-                broken_bikes = last_report.broken_bikes
-                report_date = last_report.report_date
-                data = {
-                    'number':'broken_bikes',
-                    'date':'report_date',
-                    'id':'stationId'
-                }
-                return HttpResponse(json.dumps(data))
+def see_last_report(request, station_id):
+    ''' Returns a dictionnary with data about the last report in JSON'''
+    if station_id in stations:
+        latest_reports = Report.objects.filter(station_id=station_id).order_by('-report_date')
+        if len(latest_reports) >= 1:
+            last_report = latest_reports[0]
+
+            report_date = last_report.report_date
+            broken_bikes = last_report.broken_bikes
+            available_bikes = last_report.available_bikes
+            return_dict = {
+                            'report_date':'{}'.format(datetime.strftime(report_date, "%d\%m\%Y %X")),
+                            'broken_bikes':'{}'.format(broken_bikes),
+                            'available_bikes':'{}'.format(available_bikes),
+                            'station_id':'{}'.format(station_id)
+                          }
+            return HttpResponse(json.dumps(return_dict))
         else:
-            return HttpResponse("Sorry, but #%s is not a valid station id."
-                                    % station_id)
+            return HttpResponse("There is no report yet for this station.")
+    else:
+        return HttpResponse("Sorry, but #%s is not a valid station id."
+                            % station_id)
